c1be590a9c1e1e9629aa1a62f57aacd586f4b589
Tlnk23 <52453198+Tlnk23@users.noreply.github.com>
2020-08-26 16:59:01 +0300

    last change

--- script.js
+++ script.js
@@ -6,51 +6,56 @@ window.addEventListener('DOMContentLoaded', function() {
         cardSlogan = document.querySelectorAll('.cards-section__card-slogan'),
         cardDiscription = document.querySelectorAll('.cards-section__card-discription'),
         cardCircle = document.querySelectorAll('.cards-section__circle'),
+        cardBuy = document.querySelectorAll('.cards-section__card-discription-buy'),
         cardText = ['Печень утки разварная с артишоками.', 'Головы щучьи с чесноком да свежайшая сёмгушка.', 'Филе из цыплят с трюфелями в бульоне.'];
 
         cardsList.addEventListener('click', function(cardClick) {
             let target = cardClick.target;
-            
-            if (target && target.classList.contains('cards-section__card-img')) {
-                for(let i = 0; i < cardElement.length; i++) {
-                    if (target == cardElement[i]) {
-                        cardElement[i].classList.remove('cards-section__card-img');
-                        cardElement[i].classList.add('cards-section__card-img--active');
-                        cardCircle[i].classList.remove('cards-section__circle');
-                        cardCircle[i].classList.add('cards-section__circle--active');
-                        cardDiscription[i].innerHTML = cardText[i];
-                        break;
-                    }
+
+            if (target.classList.contains('cards-section__card-discription-buy')) {
+                return;
+            } else {
+                while ((target.classList[0] != 'cards-section__card-img')) {
+                    target = target.parentNode;
                 }
-            } else if (target && target.classList.contains('cards-section__card-img--active')) {
+            }
+
+            if (target && target.classList.contains('cards-section__card-img') && target.classList.contains('cards-section__card-img--active')) {
                 for(let i = 0; i < cardElement.length; i++) {
                     if (target == cardElement[i]) {
-                        cardElement[i].classList.add('cards-section__card-img');
                         cardElement[i].classList.remove('cards-section__card-img--active');
-                        cardCircle[i].classList.add('cards-section__circle');
                         cardCircle[i].classList.remove('cards-section__circle--active');
-                        cardDiscription[i].innerHTML = 'Чего сидишь? Порадуй котэ, <a class="cards-section__card-discription-buy">купи.</a>';
+                        cardDiscription[i].innerHTML = 'Чего сидишь? Порадуй котэ, <p class="cards-section__card-discription-buy">купи.</p>';
                         cardSlogan[i].innerHTML = 'Сказочное заморское яство';
                         cardSlogan[i].classList.remove('cards-section__card-slogan--hover')
                         cardCircle[i].classList.remove('cards-section__circle--active-hover');
                         break;
                     }
                 }
-            }
-        });
-
-        cardsList.addEventListener('mouseover', function(cardClick) {
-            let target = cardClick.target;
-            
-            if (target && target.classList.contains('cards-section__card-img')) {
+            } else if (target && target.classList.contains('cards-section__card-img')) {
                 for(let i = 0; i < cardElement.length; i++) {
                     if (target == cardElement[i]) {
-                        cardCircle[i].classList.remove('cards-section__circle');
-                        cardCircle[i].classList.add('cards-section__circle--hover');
+                        cardElement[i].classList.add('cards-section__card-img--active');
+                        cardCircle[i].classList.add('cards-section__circle--active');
+                        cardDiscription[i].innerHTML = cardText[i];
                         break;
-                    }
                 }
-            } else if (target && target.classList.contains('cards-section__card-img--active')) {
+            }
+        }
+        });
+
+        cardsList.addEventListener('mouseover', function(cardHoverIn) {
+            let target = cardHoverIn.target;
+            
+            if (target.classList.contains('cards-section__card-discription-buy')) {
+                return;
+            } else {
+                while ((target.classList[0] != 'cards-section__card-img')) {
+                    target = target.parentNode;
+                }
+            }
+
+            if (target && target.classList.contains('cards-section__card-img--active')) {
                 for(let i = 0; i < cardElement.length; i++) {
                     if (target == cardElement[i]) {
                         cardCircle[i].classList.add('cards-section__circle--active-hover');
@@ -60,21 +65,29 @@ window.addEventListener('DOMContentLoaded', function() {
                         break;
                     }
                 }
-            }
-        });
-
-        cardsList.addEventListener('mouseout', function(cardClick) {
-            let target = cardClick.target;
-            
-            if (target && target.classList.contains('cards-section__card-img')) {
+            } else if (target && target.classList.contains('cards-section__card-img')) {
                 for(let i = 0; i < cardElement.length; i++) {
                     if (target == cardElement[i]) {
-                        cardCircle[i].classList.add('cards-section__circle');
-                        cardCircle[i].classList.remove('cards-section__circle--hover');
+                        cardCircle[i].classList.remove('cards-section__circle');
+                        cardCircle[i].classList.add('cards-section__circle--hover');
                         break;
                     }
                 }
-            } else if (target && target.classList.contains('cards-section__card-img--active')) {
+            }
+        });
+
+        cardsList.addEventListener('mouseout', function(cardHoverOut) {
+            let target = cardHoverOut.target;
+
+            if (target.classList.contains('cards-section__card-discription-buy')) {
+                return;
+            } else {
+                while ((target.classList[0] != 'cards-section__card-img')) {
+                    target = target.parentNode;
+                }
+            }
+            
+            if (target && target.classList.contains('cards-section__card-img--active')) {
                 for(let i = 0; i < cardElement.length; i++) {
                     if (target == cardElement[i]) {
                         cardCircle[i].classList.remove('cards-section__circle--active-hover');
@@ -84,9 +97,32 @@ window.addEventListener('DOMContentLoaded', function() {
                         break;
                     }
                 }
+            } else if (target && target.classList.contains('cards-section__card-img')) {
+                for(let i = 0; i < cardElement.length; i++) {
+                    if (target == cardElement[i]) {
+                        cardCircle[i].classList.add('cards-section__circle');
+                        cardCircle[i].classList.remove('cards-section__circle--hover');
+                        break;
+                    }
+                }
             }
         });
 
-
+    console.log(cardBuy.length);    
+    cardsList.addEventListener('click', function(cardBuyClick) {
+        let target = cardBuyClick.target;
+        console.log(target);
+        if (target.classList.contains('cards-section__card-discription-buy')) {
+            console.log(target);
+            for(let i = 0; i < cardBuy.length; i++) {
+                if (target == cardBuy[i]) {
+                    cardElement[i].classList.add('cards-section__card-img--active');
+                    cardCircle[i].classList.add('cards-section__circle--active');
+                    cardDiscription[i].innerHTML = cardText[i];
+                    break;
+            }
+        }
+        }
+    });
 
 });
